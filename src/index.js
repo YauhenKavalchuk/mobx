250e924b003c0989bf86305541115010e1c12d11
Yauhen <silent_control@tut.by>
2019-01-08 20:31:48 +0300

    Lesson 04: Observable Object & Array

--- src/index.js
+++ src/index.js
@@ -6,23 +6,24 @@ import DevTools from 'mobx-react-devtools';
 import { observable, computed } from 'mobx';
 import { observer } from 'mobx-react';
 
-const nickName = new class UserNickName {
-  @observable firstName = 'Yauhen';
-  @observable age = 30;
+const nickName = observable({
+  firstName: 'Yauhen',
+  age: 30,
 
-  @computed get nickName() {
+  get nickName() {
     console.log('Generate nickName!');
     return `${this.firstName}${this.age}`;
-  }
-}
+  },
 
-nickName.increment = function() {
-  this.age++
-}
+  increment() { this.age++ },
 
-nickName.decrement = function() {
-  this.age--
-}
+  decrement() { this.age-- },
+});
+
+const todos = observable([
+  { text: 'Learn React' },
+  { text: 'Learn MobX' },
+])
 
 @observer class Counter extends Component {
 
@@ -33,15 +34,21 @@ nickName.decrement = function() {
     return (
       <div className="App">
         <DevTools />
-        <h1>{this.props.store.nickName}</h1>
+        {/*<h1>{this.props.store.nickName}</h1>
         <h1>{this.props.store.age}</h1>
         <button onClick={this.handleDecrement}>-1</button>
-        <button onClick={this.handleIncrement}>+1</button>
+        <button onClick={this.handleIncrement}>+1</button>*/}
+
+        <ul>
+          {todos.map(({ text }) => <li key={text}>{text}</li>)}
+        </ul>
       </div>
     );
   }
 }
 
-ReactDOM.render(<Counter store={nickName} />, document.getElementById('root'));
+ReactDOM.render(<Counter store={todos} />, document.getElementById('root'));
+
+todos.push({ text: 'Learn Redux' });
 
 serviceWorker.unregister();
