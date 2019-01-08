ade27c773add085946453a7443833ef5843b5056
Yauhen <silent_control@tut.by>
2019-01-08 20:41:01 +0300

    Lesson 07: Async Requests

--- src/index.js
+++ src/index.js
@@ -1,137 +1,52 @@
 import './index.css';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { observable, computed, configure, action, decorate } from 'mobx';
+import { observable, computed, configure, action, decorate, runInAction } from 'mobx';
 import { observer } from 'mobx-react';
 import DevTools from 'mobx-react-devtools';
 configure({ enforceActions: 'observed' });
 
 class Store {
-  devsList = [
-    { name: "Jack", sp: 12 },
-    { name: "Max", sp: 10 },
-		{ name: "Leo", sp: 8 },
-  ];
-  filter: '';
+  user: null;
 
-	get totalSum() {
-    return this.devsList.reduce((sum, { sp }) => sum += sp, 0);
-  };
-
-  get topPerformer() {
-    const maxSp = Math.max(...this.devsList.map(({ sp }) => sp));
-    return this.devsList.find(({ sp, name }) => {
-      if(maxSp === sp) {
-        return name;
-      }
-    });
-  };
-
-  get filteredDevelopers() {
-    const matchesFilter = new RegExp(this.filter, "i");
-    return this.devsList.filter(({ name }) => !this.filter || matchesFilter.test(name));
+	getUser() {
+    fetch('https://randomuser.me/api/')
+      .then(res => res.json())
+      .then(json => {
+        if(json.results) {
+          //this.setUser(json.results);
+          runInAction(() => {
+            this.user = json.results[0];
+          })
+        }
+      })
   }
 
-  clearList() {
-    this.devsList = [];
-  };
-
-  addDeveloper(dev) {
-    this.devsList.push(dev);
-  };
-
-  updateFilter(value) {
-    this.filter = value;
+  setUser(results) {
+    this.user = results[0];
   }
 };
 
 decorate(Store, {
-  devsList: observable,
-  filter: observable,
-  totalSum: computed,
-  topPerformer: computed,
-  filteredDevelopers: computed,
-  clearList: action,
-  addDeveloper: action,
-  updateFilter: action,
+  user: observable,
+  getUser: action.bound,
+  setUser: action,
 })
 
 const appStore = new Store();
 
-const Row = ({ data: { name, sp } }) => {
-  return (
-		<tr>
-    	<td>{name}</td>
-    	<td>{sp}</td>
-  	</tr>
-	);
-};
-
-@observer class Table extends Component {
+@observer class App extends Component {
   render() {
     const { store } = this.props;
 
     return (
-      <table>
-        <thead>
-          <tr>
-            <td>Name:</td>
-            <td>SP:</td>
-          </tr>
-        </thead>
-        <tbody>
-          {store.filteredDevelopers.map((dev, i) => <Row key={i} data={dev} />)}
-        </tbody>
-        <tfoot>
-          <tr>
-            <td>Team SP:</td>
-            <td>{store.totalSum}</td>
-          </tr>
-          <tr>
-            <td>Top Performer:</td>
-            <td>{store.topPerformer ? store.topPerformer.name : ''}</td>
-          </tr>
-        </tfoot>
-      </table>
-		);
-  }
-}
-
-@observer class Controls extends Component {
-  addDeveloper = () => {
-    const name = prompt("The name:");
-    const sp = parseInt(prompt("The story points:"), 10);
-    this.props.store.addDeveloper({ name, sp });
-  }
-
-  clearList = () => { this.props.store.clearList(); }
-
-  filterDevelopers = ({ target: { value } }) => {
-    this.props.store.updateFilter(value);
-  }
-
-  render() {
-    return (
-			<div className="controls">
-      	<button onClick={this.clearList}>Clear table</button>
-      	<button onClick={this.addDeveloper}>Add record</button>
-        <input value={this.props.store.filter} onChange={this.filterDevelopers} />
-    	</div>
-		);
-  }
-}
-
-class App extends Component {
-  render() {
-    return (
       <div>
 				<DevTools />
-        <h1>Sprint Board:</h1>
-        <Controls store={appStore} />
-        <Table store={appStore} />
+        <button onClick={store.getUser}>Get User</button>
+        <h1>{store.user ? store.user.login.username : 'Default name'}</h1>
       </div>
     )
   }
 }
 
-ReactDOM.render(<App store={Store} />, document.getElementById('root'));
+ReactDOM.render(<App store={appStore} />, document.getElementById('root'));
