a454f8436d8b2c0598630bbbd02430bdc7f753a5
Yauhen <silent_control@tut.by>
2019-01-08 20:29:23 +0300

    Lesson 02: Observable & Observer

--- src/index.js
+++ src/index.js
@@ -1,19 +1,40 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 import * as serviceWorker from './serviceWorker';
 import DevTools from 'mobx-react-devtools';
+import { observable } from 'mobx';
+import { observer } from 'mobx-react';
 
-class App extends Component {
+const counterState = observable({
+  count: 0,
+});
+
+counterState.increment = function() {
+  this.count++
+}
+
+counterState.decrement = function() {
+  this.count--
+}
+
+@observer class Counter extends Component {
+
+  handleIncrement = () => { this.props.store.increment() };
+  handleDecrement = () => { this.props.store.decrement() };
+
   render() {
     return (
       <div className="App">
         <DevTools />
-        <h1>Hello world!</h1>
+        <h1>{this.props.store.count}</h1>
+        <button onClick={this.handleDecrement}>-1</button>
+        <button onClick={this.handleIncrement}>+1</button>
       </div>
     );
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Counter store={counterState} />, document.getElementById('root'));
 
 serviceWorker.unregister();
